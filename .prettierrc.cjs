f607fc240cd9bd6c6c74c95809cd2f2a65d893ac
dendy <lyovin.denis@gmail.com>
2024-03-24 11:03:57 +0500

    Updates

--- .prettierrc.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  bracketSpacing: true,
-  plugins: ['prettier-plugin-tailwindcss'],
-  printWidth: 80,
-  quoteProps: 'consistent',
-  singleQuote: true,
-  trailingComma: 'all',
-};
