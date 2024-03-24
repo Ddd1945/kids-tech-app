825c37c42e55540d9f5312462ca5d74b62449201
dendy <lyovin.denis@gmail.com>
2024-03-24 12:18:52 +0500

    1.0.0

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,12 +1,15 @@
 import { Link } from 'react-router-dom';
 import svg from '../../../assets/svg/404.svg';
 import './NotFoundPage.scss';
+import getEnvVars from '../../api/enviroment';
 
 export default function NotFoundPage() {
+  const { homeRoute } = getEnvVars();
+
   return (
     <div>
       <img src={svg} alt="svg" />
-      <Link to="/kids-tech-app/" className="recover">
+      <Link to={homeRoute} className="recover">
         <div className="back">Я ПОТЕРЯЛСЯ</div>
       </Link>
     </div>
